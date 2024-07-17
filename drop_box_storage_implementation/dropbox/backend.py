51a11b16f9a875f6ec5d9fa1645c861cbcb14020
sivaganapathi <sivaganapathi812002@gmail.com>
2024-07-17 17:57:47 +0530

    hey hi i comited this my file

---
+++ drop_box_storage_implementation/dropbox/backend.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import dropbox
+
+app = Flask(__name__)
+dbx = dropbox.Dropbox("unable to commit in github so here i changed my token")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files.get('file')
+    if not file:
+        return "No file selected"
+    filename = file.filename
+    if not filename:
+        return "No file selected"
+    dbx.files_upload(file.read(), f"/images/{filename}")
+    return "File uploaded successfully"
+
+if __name__ == '__main__':
+    app.run(debug=True)
